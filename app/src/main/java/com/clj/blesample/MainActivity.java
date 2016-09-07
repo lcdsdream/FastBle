0daad0d455b01185133745dcfa1a34b469e6f962
luchaodong <luchaodong@meizu.com>
2016-09-07 15:10:15 +0800

    connect with device which on the listView

--- app/src/main/java/com/clj/blesample/MainActivity.java
+++ app/src/main/java/com/clj/blesample/MainActivity.java
@@ -25,14 +25,11 @@ import com.clj.fastble.scan.ListScanCallback;
 import com.clj.fastble.utils.BluetoothUtil;
 import com.clj.fastble.utils.HexUtil;
 
-import java.security.PrivilegedAction;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,25 +37,27 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String UUID_CLIENT_CHARACTERISTIC_CONFIG_DESCRIPTOR = "00002902-0000-1000-8000-00805f9b34fb";
 
-    private static final String UUID_SERVICE_OPERATE = "0000fff0-0000-1000-8000-00805f9b34fb";
-    private static final String UUID_OPERATE_WRITE = "0000fff1-0000-1000-8000-00805f9b34fb";
+    private static final String UUID_SERVICE_OPERATE = "0000a800-0000-1000-8000-00805f9b34fb";
+    private static final String UUID_OPERATE_WRITE = "0000a802-0000-1000-8000-00805f9b34fb";
+    private static final String UUID_OPERATE_NOTIFY = "0000a802-0000-1000-8000-00805f9b34fb";
 
     private static final String SAMPLE_WRITE_DATA = "2204020b010300";
 
     private static final long TIME_OUT = 5000;                                   // 扫描超时时间
-    private static final String DEVICE_MAC_SAMPLE = "11:22:33:44:55:66";
 
     private BleManager bleManager;                                                // Ble核心管理类
     private Handler handler = new Handler(Looper.getMainLooper());
 
-
     // scan
-    private List<Map<String, Object>> scanListItem = new ArrayList<Map<String, Object>>();
+    private List<Map<String, Object>> scanListItem = new ArrayList<Map<String, Object>>(); // show in listView
     private List<BluetoothDevice> deviceList = new ArrayList<>(); // scan device list
     private List<String> deviceListData = new ArrayList<>(); // with device's adv data
 
-    private String deviceMac;
+    private String deviceMac; // target device address
     private MyListScanCallback myListScanCallback = new MyListScanCallback(TIME_OUT);
+    private BleConnGattCallback myConnGattCallback = new BleConnGattCallback();
+    private BleWriteCharacterCallback myWriteGattback = new BleWriteCharacterCallback();
+    private BleNtyCharacterCallback myNtyGattback = new BleNtyCharacterCallback();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -71,7 +70,6 @@ public class MainActivity extends AppCompatActivity {
         bleManager.init(this);
     }
 
-
     private void initView() {
 
         //scan adv
@@ -79,8 +77,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                bleManager.enableBluetooth();
+
                 if (bleManager.isInScanning()) {
                     return;
+                }
+
+                if (bleManager.isConnectingOrConnected()) {
+                    bleManager.closeBluetoothGatt();
                 }
 
                 scanListItem.clear();
@@ -102,15 +106,38 @@ public class MainActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                if (bleManager.isInScanning()) {
+                    showToast("Scanning!!");
+                    return;
+                }
+
+                if (bleManager.isConnectingOrConnected()) {
+                    if (bleManager.isConnected()) {
+                        showToast("isConnected");
+                    } else {
+                        showToast("isConnecting");
+                    }
+                    return;
+                }
+
                 ListView listView = (ListView)parent;
                 HashMap<String, Object> map = (HashMap<String, Object>) listView.getItemAtPosition(position);
                 String name = (String) map.get("name");
                 String mac = (String) map.get("mac");
-                showToast("Click : " + name + " " + mac);
+                showToast("Connect with\n" + name + " " + mac);
+
+                // connect device
+                for(final BluetoothDevice device : deviceList) {
+                    if (device.getAddress().equalsIgnoreCase(mac)) {
+                        bleManager.connectDevice(device, myConnGattCallback);
+                    }
+                }
             }
         });
     }
 
+/***********************************************************************************************************************/
     private static boolean isMainThread() {
         return Looper.myLooper() == Looper.getMainLooper();
     }
@@ -131,7 +158,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
 
     class MyListScanCallback extends ListScanCallback {
 
@@ -189,6 +215,74 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    class BleWriteCharacterCallback extends BleCharacterCallback {
+        @Override
+        public void onSuccess(BluetoothGattCharacteristic characteristic) {
+            Log.d(TAG, "写特征值成功: "
+                    + '\n' + Arrays.toString(characteristic.getValue())
+                    + '\n' + HexUtil.encodeHexStr(characteristic.getValue()));
+        }
+        @Override
+        public void onFailure(BleException exception) {
+            Log.e(TAG, "写读特征值失败: " + '\n' + exception.toString());
+            bleManager.handleException(exception);
+        }
+    }
+
+    class  BleNtyCharacterCallback extends BleCharacterCallback {
+        @Override
+        public void onSuccess(BluetoothGattCharacteristic characteristic) {
+            Log.d(TAG, "特征值Notification通知数据回调： "
+                    + '\n' + HexUtil.encodeHexStr(characteristic.getValue())
+                    + '\n' + Arrays.toString(characteristic.getValue())
+                    + "\nuuid :" + characteristic.getUuid());
+            showToast(HexUtil.encodeHexStr(characteristic.getValue()));
+        }
+        @Override
+        public void onFailure(BleException exception) {
+            Log.e(TAG, "特征值Notification通知回调失败: " + '\n' + exception.toString());
+            bleManager.handleException(exception);
+        }
+    }
+
+    class BleConnGattCallback extends BleGattCallback
+    {
+        @Override
+        public void onConnectSuccess(BluetoothGatt gatt, int status) {
+            Log.i(TAG, "连接成功！");
+            gatt.discoverServices();                // 连接上设备后搜索服务
+            showToast("连接成功 : " + gatt.getDevice().getAddress());
+        }
+
+        @Override
+        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+            Log.i(TAG, "服务被发现！");
+            BluetoothUtil.printServices(gatt);            // 打印该设备所有服务、特征值
+            bleManager.getBluetoothState();               // 打印与该设备的当前状态
+            runOnMainThread(new Runnable() {
+                @Override
+                public void run() {
+                    // set listen
+                    bleManager.notifyDevice(UUID_SERVICE_OPERATE, UUID_OPERATE_NOTIFY, UUID_CLIENT_CHARACTERISTIC_CONFIG_DESCRIPTOR, myNtyGattback);
+
+                }
+            });
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    bleManager.writeDevice(UUID_SERVICE_OPERATE, UUID_OPERATE_WRITE, UUID_CLIENT_CHARACTERISTIC_CONFIG_DESCRIPTOR,
+                            HexUtil.hexStringToBytes(SAMPLE_WRITE_DATA),
+                            myWriteGattback);
+                }
+            }, 500);
+        }
+        @Override
+        public void onConnectFailure(BleException exception) {
+            Log.i(TAG, "连接失败或连接中断：" + '\n' + exception.toString());
+            showToast("连接断开");
+            bleManager.handleException(exception);
+        }
+    }
 
     @Override
     protected void onDestroy() {
@@ -197,188 +291,4 @@ public class MainActivity extends AppCompatActivity {
         bleManager.disableBluetooth();
     }
 }
-
-/*
-        findViewById(R.id.btn_01).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                if (bluetoothDevices == null || bluetoothDevices.length < 1)
-                    return;
-                BluetoothDevice sampleDevice = bluetoothDevices[0];
-
-
-                bleManager.connectDevice(sampleDevice, new BleGattCallback() {
-                    @Override
-                    public void onConnectSuccess(BluetoothGatt gatt, int status) {
-                        Log.i(TAG, "连接成功！");
-                        gatt.discoverServices();                // 连接上设备后搜索服务
-                    }
-
-                    @Override
-                    public void onServicesDiscovered(BluetoothGatt gatt, int status) {
-                        Log.i(TAG, "服务被发现！");
-                        BluetoothUtil.printServices(gatt);            // 打印该设备所有服务、特征值
-                        bleManager.getBluetoothState();               // 打印与该设备的当前状态
-                    }
-
-                    @Override
-                    public void onConnectFailure(BleException exception) {
-                        Log.i(TAG, "连接失败或连接中断：" + '\n' + exception.toString());
-                        bleManager.handleException(exception);
-                    }
-                });
-            }
-        });
-
-        findViewById(R.id.btn_1).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                bleManager.connectDevice(
-                        DEVICE_NAME,
-                        TIME_OUT,
-                        new BleGattCallback() {
-                            @Override
-                            public void onConnectSuccess(BluetoothGatt gatt, int status) {
-                                Log.i(TAG, "连接成功！");
-                                gatt.discoverServices();                // 连接上设备后搜索服务
-                            }
-
-                            @Override
-                            public void onServicesDiscovered(BluetoothGatt gatt, int status) {
-                                Log.i(TAG, "服务被发现！");
-                                BluetoothUtil.printServices(gatt);            // 打印该设备所有服务、特征值
-                                bleManager.getBluetoothState();               // 打印与该设备的当前状态
-                            }
-
-                            @Override
-                            public void onConnectFailure(BleException exception) {
-                                Log.i(TAG, "连接失败或连接中断：" + '\n' + exception.toString());
-                                bleManager.handleException(exception);
-                            }
-                        });
-            }
-        });
-
-        findViewById(R.id.btn_2).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                bleManager.notifyDevice(
-                        UUID_SERVICE_LISTEN,
-                        UUID_LISTEN_NOTIFY,
-                        UUID_CLIENT_CHARACTERISTIC_CONFIG_DESCRIPTOR,
-                        new BleCharacterCallback() {
-                            @Override
-                            public void onSuccess(BluetoothGattCharacteristic characteristic) {
-                                Log.d(TAG, "特征值Notification通知数据回调： "
-                                        + '\n' + Arrays.toString(characteristic.getValue())
-                                        + '\n' + HexUtil.encodeHexStr(characteristic.getValue()));
-                            }
-
-                            @Override
-                            public void onFailure(BleException exception) {
-                                Log.e(TAG, "特征值Notification通知回调失败: " + '\n' + exception.toString());
-                                bleManager.handleException(exception);
-                            }
-                        });
-            }
-        });
-
-        findViewById(R.id.btn_3).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                bleManager.indicateDevice(
-                        UUID_SERVICE_LISTEN,
-                        UUID_LISTEN_INDICATE,
-                        UUID_CLIENT_CHARACTERISTIC_CONFIG_DESCRIPTOR,
-                        new BleCharacterCallback() {
-                            @Override
-                            public void onSuccess(BluetoothGattCharacteristic characteristic) {
-                                Log.d(TAG, "特征值Indication通知数据回调： "
-                                        + '\n' + Arrays.toString(characteristic.getValue())
-                                        + '\n' + HexUtil.encodeHexStr(characteristic.getValue()));
-                            }
-
-                            @Override
-                            public void onFailure(BleException exception) {
-                                Log.e(TAG, "特征值Indication通知回调失败: " + '\n' + exception.toString());
-                                bleManager.handleException(exception);
-                            }
-                        });
-            }
-        });
-
-        findViewById(R.id.btn_4).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                bleManager.writeDevice(
-                        UUID_SERVICE_OPERATE,
-                        UUID_OPERATE_WRITE,
-                        UUID_CLIENT_CHARACTERISTIC_CONFIG_DESCRIPTOR,
-                        HexUtil.hexStringToBytes(SAMPLE_WRITE_DATA),
-                        new BleCharacterCallback() {
-                            @Override
-                            public void onSuccess(BluetoothGattCharacteristic characteristic) {
-                                Log.d(TAG, "写特征值成功: "
-                                        + '\n' + Arrays.toString(characteristic.getValue())
-                                        + '\n' + HexUtil.encodeHexStr(characteristic.getValue()));
-                            }
-
-                            @Override
-                            public void onFailure(BleException exception) {
-                                Log.e(TAG, "写读特征值失败: " + '\n' + exception.toString());
-                                bleManager.handleException(exception);
-                            }
-                        });
-            }
-        });
-
-
-        findViewById(R.id.btn_6).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                bleManager.refreshDeviceCache();
-            }
-        });
-
-        findViewById(R.id.btn_7).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                bleManager.closeBluetoothGatt();
-            }
-        });
-    }
-
-    BleCharacterCallback bleCharacterCallback = new BleCharacterCallback() {
-        @Override
-        public void onSuccess(BluetoothGattCharacteristic characteristic) {
-            Log.d(TAG, "特征值Notification通知数据回调： "
-                    + '\n' + Arrays.toString(characteristic.getValue())
-                    + '\n' + HexUtil.encodeHexStr(characteristic.getValue()));
-        }
-
-        @Override
-        public void onFailure(BleException exception) {
-            Log.e(TAG, "特征值Notification通知回调失败: " + '\n' + exception.toString());
-            bleManager.handleException(exception);
-        }
-    };
-
-    private void addAndRemove() {
-
-        bleManager.notifyDevice(
-                UUID_SERVICE_OPERATE,
-                UUID_OPERATE_NOTIFY,
-                UUID_CLIENT_CHARACTERISTIC_CONFIG_DESCRIPTOR,
-                bleCharacterCallback);
-
-        bleManager.removeBleCharacterCallback(bleCharacterCallback);
-    }
-*/
 
