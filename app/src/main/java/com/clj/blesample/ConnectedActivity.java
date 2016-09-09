bde0b606128514f8442946b77773a49f66af495a
luchaodong <luchaodong@meizu.com>
2016-09-09 18:05:26 +0800

    disconnect when ConnectedACtivity destory

--- app/src/main/java/com/clj/blesample/ConnectedActivity.java
+++ app/src/main/java/com/clj/blesample/ConnectedActivity.java
@@ -195,5 +195,11 @@ public class ConnectedActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        bleManager.closeBluetoothGatt();
+    }
+
 }
 
